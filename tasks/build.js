5a7444a169d995e0e20293da7710d1d48a1c054c
clement <maboiteaspam@wanadoo.fr>
2013-10-07 00:32:27 +0800

    commit

--- tasks/build.js
+++ tasks/build.js
@@ -2,10 +2,11 @@
 
 module.exports = function(grunt) {
 
+    var path = require('path');
+    var ph_libutil = require("phantomizer-libutil");
+
     grunt.registerMultiTask("phantomizer-imgopt", "Optimize png/jpeg pictures", function () {
 
-        var ph_libutil = require("phantomizer-libutil");
-        var path = require('path');
 
         var sub_tasks = []
         var current_grunt_task = this.nameArgs;
@@ -14,15 +15,6 @@ module.exports = function(grunt) {
         var meta_factory = ph_libutil.meta;
 
         var wd = process.cwd();
-
-        function find_in_paths(paths, src){
-            for( var t in paths ){
-                if( grunt.file.exists(paths[t]+src) ){
-                    return path.resolve(paths[t]+src)
-                }
-            }
-            return false
-        }
 
 
         var options = this.options({
@@ -41,15 +33,18 @@ module.exports = function(grunt) {
         var files = options.in_files || {};
         var sub_task_options = {};
 
+        var current_task_name = "phantomizer-imgopt";
+
 
         var meta_manager = new meta_factory( wd, meta_dir );
-
 
         for( var src_file in files ){
             var out_file = out_dir+"/"+files[src_file];
             var meta_file = "/"+files[src_file]+".meta";
+            var meta_infile = src_file+".meta";
 
-            if( meta_manager.is_fresh(meta_file) == false ){
+            if( meta_manager.is_fresh(meta_file) == false
+                && meta_manager.is_fresh(meta_infile,current_task_name+src_file) == false ){
 
                 var file = find_in_paths(paths,src_file);
 
@@ -87,11 +82,56 @@ module.exports = function(grunt) {
                 grunt.config.set("imagemin", sub_task_options)
                 sub_tasks.push( "imagemin"+":"+sub_task_name )
             }else{
-                grunt.log.ok("your build is fresh !")
+                grunt.log.ok("your build is fresh !\n\t"+out_file)
             }
         }
 
         grunt.task.run( sub_tasks )
     });
+
+
+    function find_in_paths(paths, src){
+        for( var t in paths ){
+            if( grunt.file.exists(paths[t]+src) ){
+                return path.resolve(paths[t]+src)
+            }
+        }
+        return false
+    }
+
+    grunt.registerMultiTask("phantomizer-dir-imgopt", "Optimize png/jpeg pictures", function () {
+
+        var sub_tasks = []
+
+        var options = this.options({
+            optimizationLevel: 0
+            ,"progressive":true
+            ,paths: []
+        });
+
+        grunt.verbose.writeflags(options, 'Options');
+
+        var paths = options.paths;
+
+        for( var n in paths ){
+            var p = paths[n];
+            var sub_task_name = "imgmin"+sub_tasks.length;
+
+            var sub_task_options = {
+                files: [{
+                    expand: true,
+                    cwd: p,
+                    src: ['**/*.{png,jpg,jpeg,gif}'],
+                    dest: ''
+                }]
+            };
+
+            grunt.config.set("imagemin", sub_task_options)
+            sub_tasks.push( "imagemin"+":"+sub_task_name )
+        }
+
+        grunt.task.run( sub_tasks )
+    });
+
 
 };
